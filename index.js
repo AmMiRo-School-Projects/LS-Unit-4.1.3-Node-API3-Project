9cd65ae16981b98cd32ca32fe063c1362c87852a
Amos Rose <amosroseacct@gmail.com>
2020-03-12 14:33:20 -0600

    to be deployed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require("./server");
 
-const PORT = 5000;
+const port = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-  console.log(`\n** Server running on http://localhost:${PORT} **\n`);
+server.listen(port, () => {
+  console.log(`\n** Server running on http://localhost:${port} **\n`);
 });
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.use("/api/posts", logger, postRouter);
 server.use("/api/users", logger, userRouter);
 
 server.get("/", logger, (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.status(200).json({ message: "Welcome" });
 });
 
 //custom middleware
